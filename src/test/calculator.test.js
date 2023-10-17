c3476ddfcee827131f8268c85fd8aeb15e774634
raj-freemind <raj.freemind@gmail.com>
2023-10-17 06:05:56 +0545

    add some more test cases

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -1,6 +1,7 @@
 const { add, divide } = require("../service/calculator");
 
 describe('Adding Two Number', () => {
+  
   test('Adding two positive Number', () => {
     const adding = add(4, 2)
     expect(adding).toBe(6)
@@ -33,18 +34,36 @@ describe('Adding Two Number', () => {
     const adding = add(1.3, 1.2)
     expect(adding).toBeCloseTo(2.5);
     expect(adding).toBeGreaterThan(0)
-  });
+  })
+
+  //failed this test case
+  // test('Adding numbers represented as strings', () => {
+  //   const adding = add("10", "20")
+  //   expect(adding).toBe(30);
+  //   expect(adding).toBeGreaterThan(0)
+  // })
+
+  //failed this test case need to fix function
+  // test('Adding non-numeric strings', () => {
+  //   const adding = add("raj", "a")
+  //   expect(adding).toBeNaN();
+  // })
+
 })
 
 describe("Dividing a Number", () => {
+
+
   it('Dividing P/N integer by P/N integer', () => {
     const dividing = divide(4, 2)
     expect(dividing).toBeGreaterThan(0)
   })
+
   it('Dividing a nagative number by positive number', () => {
     const dividing = divide(-3, 2)
     expect(dividing).toBeLessThan(0)
   })
+
   it('Dividing a positive number by nagative number', () => {
     const dividing = divide(3, -2)
     expect(dividing).toBeCloseTo(-1.5)
@@ -59,6 +78,21 @@ describe("Dividing a Number", () => {
   it('Dividing Some number by Zero', () => {
     const dividing = divide(2, 0)
     expect(dividing).toBe(Infinity)
+  })
+
+  it('Dividing zero by zero', () => {
+    const dividing = divide(0, 0)
+    expect(dividing).toBeNaN();
+  })
+
+  it('Dividing a string representation of a number by a number', () => {
+    const dividing = divide("10", 5)
+    expect(dividing).toBe(2);
+  })
+
+  it('Dividing a number by a string', () => {
+    const dividing = divide(10, "abc")
+    expect(dividing).toBeNaN();
   })
 
 })
